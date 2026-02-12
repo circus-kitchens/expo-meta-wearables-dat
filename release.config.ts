248b42961e8f33bab08f4020f784d88a2c5963bd
Fabricio Borgobello <fborgobello@gmail.com>
2026-02-12 16:36:40 +0100

    build: restore deployment workflow

--- release.config.ts
+++ release.config.ts
@@ -8,7 +8,13 @@ const config: Options = {
     "@semantic-release/release-notes-generator",
     "@semantic-release/changelog",
     "semantic-release-pnpm",
-    "@semantic-release/git",
+    [
+      "@semantic-release/git",
+      {
+        assets: ["CHANGELOG.md", "package.json"],
+        message: "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
+      },
+    ],
     "@semantic-release/github",
   ],
 };
